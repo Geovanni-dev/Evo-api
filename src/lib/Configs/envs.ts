8ab6cf4b1c949f03c25d9c8d3f3e6c3349b820a6
Geovanni-dev <geovanni3344@outlook.com>
2026-09-25 14:32:59 -0300

    feat: add authentication modules including controller, routes, schemas, and services for Google login

--- src/lib/Configs/envs.ts
+++ src/lib/Configs/envs.ts
@@ -4,6 +4,8 @@ const envSchema = z.object({
   GEMINI_API_KEY: z.string(),
   GOOGLE_GENAI_API_KEY: z.string(),
   DEEPSEEK_KEY: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  JWT_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
